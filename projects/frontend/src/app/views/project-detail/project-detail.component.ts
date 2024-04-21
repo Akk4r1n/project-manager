ee54e38bc41977f050f31e7b2994a455f2b32b94
Akk4r1n <davidriewe01@gmail.com>
2024-04-21 16:23:47 +0200

    Implement mockup of detail page header

--- projects/frontend/src/app/views/project-detail/project-detail.component.ts
+++ projects/frontend/src/app/views/project-detail/project-detail.component.ts
@@ -3,6 +3,7 @@ import { Component, Input } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { heroArrowLeft } from '@ng-icons/heroicons/outline';
+import { Project } from '../../interfaces/domain';
 
 @Component({
   selector: 'app-project-detail',
@@ -24,6 +25,16 @@ import { heroArrowLeft } from '@ng-icons/heroicons/outline';
 })
 export class ProjectDetailComponent {
   public _uuid!: string;
+
+  public project: Project = {
+    uuid: '34ed8ba3-f7b4-4565-a5a9-b5605b0d8df4',
+    title: 'E-Commerce Platform Development',
+    description:
+      'Development of a scalable e-commerce platform with advanced search and recommendation features.',
+    createdAt: new Date('2023-03-10'),
+    ownerEmail: 'project_owner1@example.com',
+    chatUuid: 'chat1',
+  };
 
   @Input()
   set uuid(projectUuid: string) {
