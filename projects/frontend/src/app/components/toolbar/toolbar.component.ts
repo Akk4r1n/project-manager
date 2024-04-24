1c7c58f84841c4690a60f9de7cc48cb2fc28b3c3
Akk4r1n <davidriewe01@gmail.com>
2024-04-24 18:24:41 +0200

    Refactor toolbar menu

--- projects/frontend/src/app/components/toolbar/toolbar.component.ts
+++ projects/frontend/src/app/components/toolbar/toolbar.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { heroChevronDown } from '@ng-icons/heroicons/outline';
+import { heroChevronDown, heroWrench } from '@ng-icons/heroicons/outline';
+import { UsersService } from '../../services/api/users.service';
 
 @Component({
   selector: 'app-toolbar',
@@ -10,6 +11,27 @@ import { heroChevronDown } from '@ng-icons/heroicons/outline';
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  viewProviders: [provideIcons({ heroChevronDown })],
+  viewProviders: [provideIcons({ heroChevronDown, heroWrench })],
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  menuState: 'open' | 'close' = 'close';
+
+  constructor(private usersService: UsersService) {}
+
+  onAvatarClick() {
+    if (this.menuState === 'open') this.menuState = 'close';
+    else this.menuState = 'open';
+  }
+
+  onProfileClick() {
+    alert('Profile not implemented yet');
+  }
+
+  onSettingsClick() {
+    alert('Settings not implemented yet');
+  }
+
+  onLogoutClick() {
+    this.usersService.logout().subscribe((_) => {});
+  }
+}
